356de45b9532f72da0609437d30dd0275efaa253
Thomas Noguer <thomas.noguer@gmail.com>
2013-02-21 09:40:55 +0100

    Ajout du main
    A'Tuin in da place!

---
+++ src/LSystem.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Caelum
+ *
+ */
+public class LSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Bonjour tortue !");
+
+	}
+
+}
